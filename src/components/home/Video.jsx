0d459c990ec80ad058e965248a3f1081a25ef754
Sharansh Jha <143743970+sharanshjha@users.noreply.github.com>
2025-09-16 15:18:16 +0530

    Update Video.jsx

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="/public/videos/India.mp4"></video>
+        <video 
+          className='h-full w-full object-cover' 
+          autoPlay 
+          loop 
+          muted 
+          playsInline 
+          preload="auto"
+          src="/videos/India.mp4"
+        ></video>
     </div>
   )
 }
